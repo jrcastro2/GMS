9b28ccbeed8c034eb7fe9b41ddcc37a6088320a7
jrcastro2 <jrcastro9515@gmail.com>
2017-11-19 21:27:50 +0100

    mejorado vista

--- view/ejercicios/view.php
+++ view/ejercicios/view.php
@@ -12,11 +12,14 @@ $view->setVariable("title", "View Ejercicio");
 
 ?><h1 id="vistaejercicio"><?= i18n("Ejercicio").": ".htmlentities($ejercicio->getTitle()) ?></h1>
 <div id="ver-ejercicio">
+	&nbsp;
 <p >
 	<?=htmlentities($ejercicio->getContent()) ?>
 </p>
+&nbsp;
+&nbsp;
 <p>
-	<?= i18n("Numero de series").": ".$ejercicio->getSeries() ?> <?= i18n("Numero de repeticiones").": ".htmlentities($ejercicio->getRepeticiones()) ?>
+	<?= i18n("Series").": ".$ejercicio->getSeries() ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <?= i18n("Repeticiones").": ".htmlentities($ejercicio->getRepeticiones()) ?>
 </p>
 <p>
 
